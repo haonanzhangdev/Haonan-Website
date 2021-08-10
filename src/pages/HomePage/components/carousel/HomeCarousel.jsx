bed8198c634c0ff8a80acf2c3c019d80fc964fff
Haonan Zhang <conanzahn@gmail.com>
2021-08-11 03:42:35 +1000

    update homepage carousel

--- src/pages/HomePage/components/carousel/HomeCarousel.jsx
+++ src/pages/HomePage/components/carousel/HomeCarousel.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Carousel} from "react-bootstrap";
-import slide1 from '../../../../assets/img/slide1.png';
+import aboutme from '../../../../assets/img/about.jpg';
+import site from '../../../../assets/img/site.jpg';
+import resume from '../../../../assets/img/resume.jpg';
 import slide2 from '../../../../assets/img/slide2.png';
 import slide3 from '../../../../assets/img/slide3.jpg';
 
@@ -9,8 +11,8 @@ const CrslStyle = styled.div`
     /* border-bottom: 1px solid #dadada; */
     margin-top:3em;
     text-align: center;
-    margin-left: 200px;
-    margin-right: 200px;
+    margin-left: 220px;
+    margin-right: 220px;
 `;
 
 const HomeCarousel = () =>(
@@ -19,35 +21,56 @@ const HomeCarousel = () =>(
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src={slide1}
-                alt="About Me"
+                src={site}
+                alt="About This Site"
                 />
                 <Carousel.Caption>
-                <h5>About Me</h5>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <h5>About This Site</h5>
                 </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src={slide2}
-                alt="Second slide"
+                src={aboutme}
+                alt="About Me"
+                />
+                
+                <Carousel.Caption>
+                <h5>About Me</h5>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={resume}
+                alt="My Resume"
                 />
                 <Carousel.Caption>
                 <h5>My Resume</h5>
-                <p>Lorem </p>
                 </Carousel.Caption>
             </Carousel.Item>
+
             <Carousel.Item>
                 <img
                 className="d-block w-100"
                 src={slide3}
-                alt="Third slide"
+                alt="My Projects"
                 />
                 <Carousel.Caption>
                 <h5>My Projects</h5>
-                <p>Praesent commodo cursus magna</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={slide3}
+                alt="Contact Me"
+                />
+                <Carousel.Caption>
+                <h5>Contact Me</h5>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
